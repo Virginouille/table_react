a3faad0c9dcb2c61ae64a7e9b78e0704dcb27049
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-09-23 16:34:27 +0200

    avanccement table

---
+++ src/components/Table.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+
+export function Table() {
+
+    const [text, setText] = useState("")
+
+    function isEmpty() {
+
+        return text.trim() === ""
+        console.log(text)
+    }
+
+    return (
+
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">Text</th>
+                    <th scope="col">Bool</th>
+                    <th scope="col">Auto Complet</th>
+                    <th scope="col">Select</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td className="w-50"><input
+                        type="text"
+                        className="bg-white"
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                        placeholder="lorem"></input></td>
+                    <td className="w-50"><select className="form-select">
+                        <option value="1">Oui</option>
+                        <option value="2">Non</option>
+                    </select></td>
+
+                    <td className="container">
+                        <div className="row">
+                            <div style={{ gridTemplateColumns: "1fr 1fr" }} className=" col mb-3 d-flex d-grid gap-3">
+                                <h6><span className="p-2 badge text-bg-secondary">Test 1</span></h6>
+                                <h6><span className="p-2 badge text-bg-secondary">Test 2</span></h6>
+                            </div>
+                        </div>
+                    </td>
+                    <td><select className="form-select" label="3/3">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select></td>
+
+                </tr>
+            </tbody>
+        </table >
+
+    )
+}
